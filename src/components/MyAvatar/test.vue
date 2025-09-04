0b805504b600184430e37ad7d95d6cee838607ee
chenxiaodong <2740318112@qq.com>
2025-09-04 15:48:17 +0800

    init commit

---
+++ src/components/MyAvatar/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MyAvatar url="http://mdrs.yuanjin.tech/img/20201031141350.jpg" :size="300" />
+  </div>
+</template>
+
+<script>
+import MyAvatar from "./";
+export default {
+  components: {
+    MyAvatar,
+  },
+};
+</script>
+
+<style></style>
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue-cli的配置文件
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://test.my-site.com",
+      },
+    },
+  },
+  configureWebpack: require("./webpack.config"),
+};
+
